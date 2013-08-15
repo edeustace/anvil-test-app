5e0955a5e62f2eb85d8992d1af91cf752b2fd077
edeustace <ed.eustace@gmail.com>
2013-08-15 09:28:45 +0200

    first commit

---
+++ app/controllers/Application.scala
@@ -0,0 +1,41 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+
+object Application extends Controller {
+
+  def index = Action {
+
+    import play.api.Logger
+    import play.api.libs.json._
+    import play.api.libs.functional.syntax._
+
+    implicit val personFormat : Format[Person] = Json.format[Person]
+
+    println(personFormat)
+     val json = """
+      {
+        "name" : "Ed",
+        "age" : 100,
+        "lovesChocolate" : true,
+        "phone" :
+          { "number" : "1", "phoneType" : "home"}
+
+      }
+     """
+
+    //this format will be implicitly used by the following from/toJson functions
+    val person:JsResult[Person] = Json.fromJson(Json.parse(json)) //JsResult is like JsSucces/JsError
+
+     Logger.info(person.toString)
+    person match {
+      case JsSuccess(p,_) => {
+        Logger.info(p.toString)
+        Ok(Json.toJson(p))
+      }
+      case _ => Ok("")
+    }
+  }
+
+}
---
+++ app/controllers/Person.scala
@@ -0,0 +1,4 @@
+package controllers
+
+case class Person(name: String, age: Int, lovesChocolate: Boolean)//phone : Phone)
+case class Phone(number:String, phoneType: String)
